1d2b62502f630158ad1d34d66852c6373c43337e
jose ortiz <freygeth@hotmail.com>
2018-11-09 14:24:24 -0500

    Update setup.py

--- mymathmodule/setup.py
+++ mymathmodule/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
-# Nombre el módulo y archivos que contienen el código de fuente.
+
 module = Extension("mymath", sources=["mymathmodule.c"])
-# Nombre del paquete, versión, descripción y una lista con las extensiones.
+
 setup(name="MyMath",
       version="1.0",
       description="Basic math functions.",
